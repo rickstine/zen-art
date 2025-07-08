96ecdab5708c361165a668e580b961dfa134f008
rickstine <gowravkumar8@gmail.com>
2025-07-08 13:26:04 +0530

    function to check if a given string is a palindrome

---
+++ src/Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        if (str.equals(reversed)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
