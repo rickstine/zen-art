a46612840e6bb685ca1545fd4b493c9bb8190ee9
rickstine <gowravkumar8@gmail.com>
2025-07-10 19:32:56 +0530

    Fibonacci program

---
+++ src/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10;
+        int first = 0;
+        int second = 1;
+        System.out.print("Fibonacci sequence: " + first + " " + second + " ");
+        for (int i = 3; i <= n; i++) {
+            int next = first + second;
+            System.out.print(next + " ");
+            first = second;
+            second = next;
+        }
+    }
+}
